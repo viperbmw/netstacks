f000d11f5c42402867c6f494ff9f7aa2d81608c7
viperbmw <casey@cwdavis.net>
2025-12-02 23:32:57 +0000

    delete netstacker

--- netstacker/netstacker/backend/core/driver/__init__.py
+++
@@ -1,3 +0,0 @@
-from netstacker.backend.core.driver.driver_auto_loader import driver_auto_loader
-
-driver_map = driver_auto_loader()
--- netstacker/netstacker/backend/core/manager/__init__.py
+++
@@ -1,3 +0,0 @@
-from netstacker.backend.core.manager.netstacker_manager import NetpalmManager
-
-ntplm = NetpalmManager()
